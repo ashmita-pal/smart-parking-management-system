da581d3ffe3a3530d4d128eb0880e2fbc97ac54f
Ashmita Pal <ashmitapal3010@gmail.com>
2026-08-18 17:27:12 +0530

    feat(frontend): initialize React application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-400 flex justify-center align-middle">
+        Tailwind Setup Done
+      </h1>
+    </>
+  )
+}
+
+export default App
+
